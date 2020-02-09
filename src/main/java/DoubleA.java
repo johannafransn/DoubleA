e75d393bf0edc6c14614cfbda1e43fa99857a222
johannafransn <adrina0002@gmail.com>
2020-02-09 12:41:01 -0800

    Init commit

--- src/main/java/DoubleA.java
+++ src/main/java/DoubleA.java
@@ -1,31 +1,61 @@
 import java.util.Scanner;
 
-public class DoubleA
+public class DoubleA {
 
-{
-    public static void main(String Args[])
-    {
 
-        // Read the command-line input
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String Args[]) {
+        Scanner sc = new Scanner(System.in);
 
-        // get their input as a String
-        String input = scanner.next();
+        // String input
+        String input = sc.nextLine();
 
-        boolean isPresent = input.indexOf("aa") != -1 ? true : false;
+        final int outState0 = 0;
+        final int outState1 = 1;
+        final int outState2 = 2;
+        int state = 0;
 
-        if(isPresent)
-        {
-            System.out.println("NO");
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+
+
+            switch (state)
+            {
+                case outState0:
+                    if (c == 'a') {
+                        state = outState1;
+                    } else {
+                       state = outState0;
+                    }
+                    break;
+
+                case outState1:
+                    if(c == 'a')
+                    {
+                        state = outState2;
+                    }
+                    else
+                    {
+                        state = outState0;
+                    }
+                    break;
+
+                case outState2:
+                    if(c == 'a')
+                    {
+                        state = outState2;
+                    }
+                    else
+                    {
+                       state = outState2;
+                    }
+                    break;
+
+            }
+
         }
-        else
-        {
-            System.out.println("YES");
-        }
 
-
+        if(state == outState0 || state == outState1) System.out.println("YES");
+        else System.out.println("NO");
     }
-
-
 
 }
